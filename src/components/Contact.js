eeff1f0e0b8331d44c56c9103a5868822575eeb2
Allaudin Ali Ahmad <ahmadcrisp@gmail.com>
2023-11-20 02:25:54 +0500

    controlled react forms

---
+++ src/components/Contact.js
@@ -0,0 +1,168 @@
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
+import "../css/styles.css";
+
+const Contact = () => {
+  const [formData, setFormData] = useState({
+    username: "",
+    email: "",
+    phone: "",
+    message: ""
+  });
+
+  const [showSuccessMessage, setShowSuccessMessage] = useState(false);
+  const [phoneError, setPhoneError] = useState("");
+  const [emailError, setEmailError] = useState("");
+
+  const handleInputChange = (e) => {
+    const { name, value } = e.target;
+    setFormData({
+      ...formData,
+      [name]: value
+    });
+
+    if (name === "phone") {
+      if (!/^\d+$/.test(value)) {
+        setPhoneError("Phone number must contain only numbers.");
+      } else if (value.length !== 11) {
+        setPhoneError("Phone number must be exactly 11 digits.");
+      } else {
+        setPhoneError("");
+      }
+    }
+    if (name === "email") {
+      const emailPattern = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+      setEmailError(emailPattern.test(value) ? "" : "Invalid email address.");
+    }
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (phoneError || emailError) {
+      return;
+    }
+    console.log("Form data submitted:", formData);
+    setShowSuccessMessage(true);
+  };
+
+  return (
+    <>
+      <nav className="navbar bg-dark">
+        <div className="container">
+          <h1 className="logo lg-heading text-light">WT</h1>
+          <ul className="nav-items">
+            <li className="nav-item">
+              <Link to="/">Home</Link>
+            </li>
+            <li className="nav-item">
+              <Link to="/about">About</Link>
+            </li>
+            <li className="nav-item">
+              <Link to="/contact">Contact</Link>
+            </li>
+          </ul>
+        </div>
+      </nav>
+
+      <section className="contact-form">
+        <div className="container">
+          <div className="form-wrapper">
+            <div className="company-address">
+              <div className="address-group">
+                <i className="fas fa-map-marker-alt fa-3x text-red"></i>
+                <h2 className="text-gray md-heading">Location</h2>
+                <p>#2661 Janakpuri Colony, city name, town name</p>
+              </div>
+              <div className="address-group">
+                <i className="far fa-envelope fa-3x text-red"></i>
+                <h2 className="text-gray md-heading">Email</h2>
+                <p>harshitvashisth@mycompany.com</p>
+              </div>
+              <div className="address-group">
+                <i className=" fas fa-phone-square-alt fa-3x text-red"></i>
+                <h2 className="text-gray md-heading">Call</h2>
+                <p>+910101010101</p>
+              </div>
+              <img
+                src={require("../img/company-img.jpg")}
+                alt="Company Image"
+              />
+            </div>
+            <form onSubmit={handleSubmit} className="form">
+              <h1 className="lg-heading text-black">Contact Us</h1>
+              <p className="text-gray">
+                Let us know your questions, suggestions, and concerns by filling
+                out the contact form below.
+              </p>
+              <div className="form-group">
+                <label htmlFor="username">Username</label>
+                <input
+                  type="text"
+                  name="username"
+                  id="username"
+                  value={formData.username}
+                  onChange={handleInputChange}
+                  required
+                />
+              </div>
+              <div className="form-group">
+                <label htmlFor="email">Email</label>
+                <input
+                  type="email"
+                  name="email"
+                  id="email"
+                  value={formData.email}
+                  onChange={handleInputChange}
+                  required
+                />
+                {emailError && <p className="error-message">{emailError}</p>}
+              </div>
+              <div className="form-group">
+                <label htmlFor="phone">Phone</label>
+                <input
+                  type="text"
+                  name="phone"
+                  id="phone"
+                  value={formData.phone}
+                  onChange={handleInputChange}
+                  required
+                />
+                {phoneError && <p className="error-message">{phoneError}</p>}
+              </div>
+              <div className="form-group">
+                <label htmlFor="message">Message</label>
+                <textarea
+                  name="message"
+                  id="message"
+                  value={formData.message}
+                  onChange={handleInputChange}
+                  required
+                />
+              </div>
+              <button type="submit" className="form-btn">
+                Submit
+              </button>
+            </form>
+
+            {showSuccessMessage && (
+              <div className="success-message">
+                <p>Form submitted successfully!</p>
+              </div>
+            )}
+          </div>
+        </div>
+      </section>
+
+      <footer className="footer">
+        <div className="social-media-links">
+          <i className="fab fa-facebook fa-4x"></i>
+          <i className="fab fa-twitter fa-4x"></i>
+          <i className="fab fa-instagram fa-4x"></i>
+        </div>
+        <p>World Travel &copy; 2020, All Rights Reserved</p>
+      </footer>
+    </>
+  );
+};
+
+export default Contact;
---
+++ src/components/Home.js
@@ -0,0 +1,126 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import "../css/styles.css";
+import showcasePhoto3 from "../img/showcase-photo3.jpg";
+import showcasePhoto1 from "../img/showcase-photo1.jpg";
+
+const Home = () => {
+  return (
+    <>
+      <header className="header">
+        <nav className="navbar">
+          <div className="container">
+            <h1 className="logo lg-heading text-light">WT</h1>
+            <ul className="nav-items">
+              <li className="nav-item">
+                <Link to="/">Home</Link>
+              </li>
+              <li className="nav-item">
+                <Link to="/about">About</Link>
+              </li>
+              <li className="nav-item">
+                <Link to="/contact">Contact</Link>
+              </li>
+            </ul>
+          </div>
+        </nav>
+        <div className="header-content">
+          <h1 className="lg-heading text-light main-heading">
+            travel the world
+          </h1>
+          <p className="text-light">
+            travel the world, experience the greatness, it's the best gift given
+            by nature
+          </p>
+          <a href="#" className="btn btn-primary text-red md-heading">
+            Explore Places
+          </a>
+        </div>
+      </header>
+
+      <section className="showcase">
+        <div className="container">
+          <div className="row row1">
+            <div className="img-box">
+              <img src={showcasePhoto3} alt="Image Description" />
+            </div>
+            <div className="text-box">
+              <h2 className="lg-heading text-black">DEGANVY, U.K</h2>
+              <p className="text-gray">
+                Lorem ipsum dolor, sit amet consectetur adipisicing elit.
+                Impedit incidunt nulla a corporis eveniet pariatur maiores, id
+                quis, totam dolore praesentium facere consequatur rem, fuga
+                minus! Rerum, dolorem praesentium. Nemo?
+              </p>
+              <a href="/about" className="btn btn-secondary">
+                More
+              </a>
+            </div>
+          </div>
+          <div className="row row2">
+            <div className="img-box">
+              <img src={showcasePhoto1} alt="Image Description" />
+            </div>
+            <div className="text-box">
+              <h2 className="lg-heading text-black">DESERT, EGYPT</h2>
+              <p className="text-gray">
+                Lorem, ipsum dolor sit amet consectetur adipisicing elit.
+                Maiores, amet ratione, quas laborum porro, explicabo vero ipsum
+                laudantium nesciunt eos illum pariatur obcaecati fugiat
+                asperiores nulla adipisci. Porro, atque itaque.
+              </p>
+              <a href="/about" className="btn btn-secondary">
+                More
+              </a>
+            </div>
+          </div>
+        </div>
+      </section>
+
+      <section className="features">
+        <div className="container">
+          <div className="box-wrapper">
+            <div className="box box-1">
+              <i className="fas fa-route fa-2x text-red"></i>
+              <h2 className="md-heading">Adventure</h2>
+              <p>
+                Lorem ipsum dolor, sit amet consectetur adipisicing elit.
+                Possimus, quia. Corporis dolores ipsa a veniam molestias beatae,
+                fuga ex laboriosam.
+              </p>
+            </div>
+            <div className="box box-2">
+              <i className="fas fa-strikethrough fa-2x"></i>
+              <h2 className="md-heading">Less Price</h2>
+              <p>
+                Lorem ipsum dolor, sit amet consectetur adipisicing elit.
+                Possimus, quia. Corporis dolores ipsa a veniam molestias beatae,
+                fuga ex laboriosam.
+              </p>
+            </div>
+            <div className="box box-3">
+              <i className="fas fa-user-check fa-2x text-red"></i>
+              <h2 className="md-heading">Experience</h2>
+              <p>
+                Lorem ipsum dolor, sit amet consectetur adipisicing elit.
+                Possimus, quia. Corporis dolores ipsa a veniam molestias beatae,
+                fuga ex laboriosam.
+              </p>
+            </div>
+          </div>
+        </div>
+      </section>
+
+      <footer className="footer">
+        <div className="social-media-links">
+          <i className="fab fa-facebook fa-4x"></i>
+          <i className="fab fa-twitter fa-4x"></i>
+          <i className="fab fa-instagram fa-4x"></i>
+        </div>
+        <p>World Travel &copy; 2020, All Rights Reserved</p>
+      </footer>
+    </>
+  );
+};
+
+export default Home;
